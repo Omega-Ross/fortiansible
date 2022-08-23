3ed5358ceaa3c09da8cbdd6aecd78559a9f304b2
Omega-Ross <111476485+Omega-Ross@users.noreply.github.com>
2022-08-23 14:59:40 +1200

    Delete addinterface.py

--- addinterface.py
+++
@@ -1,8 +0,0 @@
-import json
-import subprocess
-from pprint import pp
-
-test = subprocess.Popen(["ansible-playbook", "addinterface.yaml", "-i", "hosts.ini", "-vvv"], stdout=subprocess.PIPE)
-output = test.communicate()[0]
-
-print(type(output))
